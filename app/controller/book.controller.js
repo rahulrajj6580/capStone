a4339e9a3c8f7f996579536900dfe9251a657109
rahulrajj6580 <rahulrajj6580@gmail.com>
2025-09-26 08:22:04 +0530

    completed get post and put method without validation.

--- app/controller/book.controller.js
+++ app/controller/book.controller.js
@@ -1,27 +1,60 @@
+const { request } = require("express");
 const db = require("../models")
 const book = db.book
 
-exports.add = async(request, response) =>{
+exports.addBooks = async (request, response) => {
 
-try{
+    try {
 
-    if(!request.body){
-        new Error("body cant be empty")
-        return;
-    }    
-    const  newBook = new book({
-        title : request.body.title,
-        author: request.body.title,
-        genre : request.body.genre,
-        description : request.body.description,
-        year : request.body.year
-    }) 
+        const newBook = new book({
+            title: request.body.title,
+            author: request.body.title,
+            genre: request.body.genre,
+            description: request.body.description,
+            year: request.body.year
+        })
 
-    const data = await newBook.save(newBook);
-    response.send(data)
+        const data = await newBook.save(newBook);
+        return response.status(200).send(data);
 
-}catch(err){
-    console.error(err);
-}
+    } catch (err) {
+        response.status(500).send({ message: "error in adding a new record" });
+    }
 
 };
+
+exports.getAllBooks = async (_, response) => {
+    try {
+        const data = await book.find();
+        return response.send(data);
+
+    } catch (err) {
+        response.status(200).status(400).send({ message: "error in getting records" });
+    }
+
+}
+
+exports.getBookById = async (request, response) => {
+    try {
+        const id = request.params.id;
+        const data = await book.findById(id)
+        return response.status(200).send(data);
+
+    } catch (err) {
+        response.status(404).send({ message: "error in getting the book by id" })
+    }
+}
+
+exports.updateBookById = async (request,response) =>{
+    try{
+        const id = request.params.id;
+        const updateData = request.body        
+        const data = await book.findByIdAndUpdate(id,updateData, {new : true})
+        if(!data){
+            return response.status(404).send({message:"book not found"});
+        }
+        response.status(200).send(data);
+    }catch(err){
+        response.status(500).send({message: "failed with updating record"});
+    }
+}
--- app/routes/book.route.js
+++ app/routes/book.route.js
@@ -1,5 +1,13 @@
 const controller = require("../controller/book.controller")
+const paths = `/api/books`
 
 module.exports = (app) =>{
-    app.post("/api/books",controller.add);
+    app.post(paths,controller.addBooks);
+
+    app.get(paths,controller.getAllBooks);
+
+    app.get(`${paths}/:id`,controller.getBookById);
+
+    app.put(`${paths}/:id`,controller.updateBookById);
+
 }
